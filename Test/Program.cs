271ee210b74be75cc5f013f99839487ea14b728a
MTsocketAPI <137100299+MTsocketAPI@users.noreply.github.com>
2024-10-12 12:44:48 +0200

    Add files via upload

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,4 @@
 ﻿using MTsocketAPI.MT5;
-using System.Security.Cryptography;
 
 namespace Test
 {
@@ -7,23 +6,83 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            
             try
             {
-                Console.WriteLine("#### MTsocketAPI C# Library ####");
+				//Console.WriteLine("#### MTsocketAPI C# Library ####");
+                
+				MTsocketAPI.MT5.Terminal mt5 = new MTsocketAPI.MT5.Terminal();
+                mt5.Connect();
 
-                MTsocketAPI.MT5.Terminal mt5 = new MTsocketAPI.MT5.Terminal();
-
-                mt5.OnConnect += Tt_OnConnect;
-                mt5.OnPrice += Tt_OnPrice;
-                mt5.OnOHLC += Tt_OnOHLC;
-                mt5.OnOrderEvent += Tt_OnOrderEvent;
-
+                
+                
+                List<MTsocketAPI.MT5.OHLC_Req> ohlc_list = new List<MTsocketAPI.MT5.OHLC_Req>();
+				MTsocketAPI.MT5.OHLC_Req req = new MTsocketAPI.MT5.OHLC_Req();
+                req.SYMBOL = "EURUSD";
+                req.TIMEFRAME = TimeFrame.PERIOD_M1;
+				ohlc_list.Add(req);
+				MTsocketAPI.MT5.OHLC_Req req2 = new MTsocketAPI.MT5.OHLC_Req();
+				req2.SYMBOL = "GBPUSD";
+				req2.TIMEFRAME = TimeFrame.PERIOD_M1;
+                req2.DEPTH = 3;
+				ohlc_list.Add(req2);
+                
+                bool result = mt5.TrackOHLC(ohlc_list);
+                //double valor = mt4.Custom_Indicator("EURUSD", MTsocketAPI.MT4.TimeFrame.PERIOD_M5, "Momentum", 0, 0, new List<string> { "14", "0" });
+                //var history = mt4.GetOrderHistory(DateTime.Now.AddDays(-10), DateTime.Now.AddDays(1));
+                
+                /*
+                Terminal mt5 = new Terminal();
+				//mt5.OnPrice += Mt5_OnPrice;
+				mt5.OnMarketDepth += Mt5_OnMarketDepth;
                 if (!mt5.Connect())
+                {
+                    Console.WriteLine("Connect failed. Please check that MTsocketAPI is running");
+                    return;
+                }
+
+                mt5.TrackMarketDepth(new List<string>() { "EURUSD", "GBPUSD" });
+                */
+				//var result = mt5.GetSymbolInfo("EURUSD");
+
+                //CalendarList pepito = mt5.CalendarList(DateTime.Now.AddDays(-3), DateTime.Now.AddDays(-2));
+
+				//List<double> AlligatorLine1 = mt5.Custom_Indicator("EURUSD", TimeFrame.PERIOD_M15, "Examples\\Alligator", 0, 1, new List<string> { "13", "8", "8", "5", "5", "3", "MODE_SMMA", "PRICE_MEDIAN" });
+				//List<double> AlligatorLine2 = mt5.Custom_Indicator("EURUSD", TimeFrame.PERIOD_M15, "Examples\\Alligator", 1, 1, new List<string> { "13", "8", "8", "5", "5", "3", "MODE_SMMA", "PRICE_MEDIAN" });
+				//List<double> AlligatorLine3 = mt5.Custom_Indicator("EURUSD", TimeFrame.PERIOD_M15, "Examples\\Alligator", 2, 1, new List<string> { "13", "8", "8", "5", "5", "3", "MODE_SMMA", "PRICE_MEDIAN" });
+
+    //            Console.WriteLine("AlligatorLine1: " + AlligatorLine1[0]);
+				//Console.WriteLine("AlligatorLine2: " + AlligatorLine2[0]);
+				//Console.WriteLine("AlligatorLine3: " + AlligatorLine3[0]);
+
+				//mt5.TrackPrices(new List<string>() { "EURUSD","GBPUSD" });
+
+				//Console.ReadKey();
+				//List<Asset> list = mt5.GetSymbolList();
+				//List<string> symbols = mt5.GetSymbolList().Where(x => x.TRADE_MODE > 0).Select(x => x.NAME.ToString()).ToList();
+				//mt5.TrackPrices(symbols);
+				//mt5.TrackPrices(mt5.GetSymbolList());
+
+                /*
+                mt5Master.OnConnect += Tt_OnConnect;
+                mt5Master.OnPrice += Tt_OnPrice;
+                mt5Master.OnOHLC += Tt_OnOHLC;
+                mt5Master.OnOrderEvent += Tt_OnOrderEvent;
+
+                if (!mt5Master.Connect())
                 {
                     Console.WriteLine("Error connecting to MTsocketAPI. Please check if MT5 is running and MTsocketAPI started successfully");
                     return;
                 }
 
+                if (!mt5Slave.Connect("localhost",70,71))
+                {
+                    Console.WriteLine("Error connecting to MTsocketAPI. Please check if MT5 is running and MTsocketAPI started successfully");
+                    return;
+                }
+
+                mt5Master.TrackOrderEvent(true);
+                /*
                 List<Asset> list = mt5.GetSymbolList();
                 List<string> symbols = new List<string>();
 
@@ -79,6 +138,12 @@ namespace Test
                 Console.ReadKey(true);
                 TradeResult close = mt5.OrderClose(buy.ORDER);
                 Console.WriteLine("Result:" + close.ToString());
+                */
+                //bool result = mt5.TrackOHLC(new List<string>() { asset.NAME },TimeFrame.PERIOD_M1);
+                //bool result2 = mt5.TrackPrices(new List<string>() { asset.NAME });
+                //bool result3 = mt5.TrackOrderEvent(true);
+
+                Console.ReadKey();
 
                 Console.WriteLine("Finished!");
             }
@@ -87,26 +152,74 @@ namespace Test
                 Console.WriteLine($"Error: {ex.Message}");
                 return;
             }
+
+            //do
+            //{
+            //    System.Threading.Thread.Sleep(1000);
+            //} while (true);
         }
 
-        private static void Tt_OnOrderEvent(object? sender, OrderEvent e)
-        {
-            Console.WriteLine(e.TRADE_TRANSACTION.ToString());
-        }
+		private static void Mt5_OnMarketDepth(object? sender, MarketDepth e)
+		{
+			Console.WriteLine("Market Depth: " + e.ToString());
+		}
 
-        private static void Tt_OnOHLC(object? sender, OHLC_Msg e)
+		private static void Mt5_OnPrice(object? sender, Quote e)
         {
-            Console.WriteLine($"OHLC: {e.OHLC[0].TIME} {e.SYMBOL} Open: {e.OHLC[0].OPEN} High: {e.OHLC[0].HIGH} Low: {e.OHLC[0].LOW} Close:{e.OHLC[0].CLOSE}");
+            Console.WriteLine("Quote: " + e.ToString());
         }
+        /*
+private static void Mt5_OnPrice(object? sender, Quote e)
+{
+   Console.WriteLine("Quote: " + e.ToString());
+}
 
-        private static void Tt_OnPrice(object? sender, Quote e)
-        {
-            Console.WriteLine($"Time: {e.TIME} Symbol: {e.SYMBOL} Ask: {e.ASK} Bid: {e.BID}");
-        }
+static Dictionary<long,long> opened = new Dictionary<long,long>();
 
-        private static void Tt_OnConnect(object? sender, EventArgs e)
-        {
-            Console.WriteLine($"Connected!");
-        }
+private static void Tt_OnOrderEvent(object? sender, OrderEvent e)
+{
+   if (e.TRADE_RESULT != null)
+   {
+       //Console.WriteLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff") + " - " + e.ToString());
+
+       if (e.TRADE_REQUEST != null)
+       {
+           if (e.TRADE_REQUEST.ACTION == "TRADE_ACTION_DEAL") //Open or close position
+           {
+               if (e.TRADE_REQUEST.POSITION == 0)
+               {
+                   TradeResult tr = mt5Slave.SendOrder(e.TRADE_REQUEST.SYMBOL, e.TRADE_REQUEST.VOLUME, Enum.Parse<OrderType>( e.TRADE_REQUEST.TYPE));
+                   opened.Add(e.TRADE_REQUEST.ORDER, tr.ORDER);
+                   Console.WriteLine("New position: " + e.TRADE_REQUEST.ORDER + " Symbol: " + e.TRADE_REQUEST.SYMBOL + " Volume: " + e.TRADE_REQUEST.VOLUME);
+               }
+               else
+               {
+                   mt5Slave.OrderClose(opened.FirstOrDefault(t => t.Key == e.TRADE_REQUEST.POSITION).Value);
+                   Console.WriteLine("Position closed: " + e.TRADE_REQUEST.POSITION + " Symbol: " + e.TRADE_REQUEST.SYMBOL + " Volume: " + e.TRADE_REQUEST.VOLUME);
+               }
+           }
+           else if (e.TRADE_REQUEST.ACTION == "TRADE_ACTION_SLTP") //Update SL or TP
+           {
+               mt5Slave.OrderModify(opened.FirstOrDefault(t => t.Key == e.TRADE_REQUEST.POSITION).Value, e.TRADE_REQUEST.SL, e.TRADE_REQUEST.TP);
+               Console.WriteLine("SL or TP updated!");
+           }
+       }
+   }
+}
+
+private static void Tt_OnOHLC(object? sender, OHLC_Msg e)
+{
+   Console.WriteLine($"OHLC: {e.OHLC[0].TIME} {e.SYMBOL} Open: {e.OHLC[0].OPEN} High: {e.OHLC[0].HIGH} Low: {e.OHLC[0].LOW} Close:{e.OHLC[0].CLOSE}");
+}
+
+private static void Tt_OnPrice(object? sender, Quote e)
+{
+   Console.WriteLine($"Time: {e.TIME} Symbol: {e.SYMBOL} Ask: {e.ASK} Bid: {e.BID}");
+}
+
+private static void Tt_OnConnect(object? sender, EventArgs e)
+{
+   Console.WriteLine($"Connected!");
+}*/
     }
 }
